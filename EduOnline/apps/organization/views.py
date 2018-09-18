3aaf4ab35664855c856d4b98e6cec299b8f71ea6
ZhiqiKou <935939006@qq.com>
2018-09-18 13:52:34 +0800

    讲师详情页

--- EduOnline/apps/organization/views.py
+++ EduOnline/apps/organization/views.py
@@ -4,7 +4,7 @@ from django.views.generic import View
 from pure_pagination import Paginator,EmptyPage,PageNotAnInteger
 from django.http import HttpResponse
 
-from .models import CourseOrg, CityDict
+from .models import CourseOrg, CityDict, Teacher
 from .forms import UserAskForm
 from courses.models import Course
 from operation.models import UserFavorite
@@ -175,3 +175,58 @@ class AddFavView(View):
                 return HttpResponse("{'status': 'success', 'msg': '已收藏'}", content_type='application/json')
             else:
                 return HttpResponse("{'status': 'fail', 'msg': '收藏出错'}", content_type='application/json')
+
+
+class TeacherListView(View):
+    """
+    课程讲师列表页
+    """
+    def get(self, request):
+        all_teachers = Teacher.objects.all()
+
+        # 排序
+        sort = request.GET.get('sort', '')
+        if sort:
+            if sort == 'hot':
+                all_teachers = all_teachers.order_by('-click_nums')
+
+        sorted_teacher = Teacher.objects.all().order_by('-click_nums')[:3]
+
+        # 对课程讲师进行分页
+        try:
+            page = request.GET.get('page', 1)
+        except:
+            page = 1
+        # 每页5个数据
+        p = Paginator(all_teachers, 5, request=request)
+        teachers = p.page(page)
+        return render(request, 'teachers-list.html', {
+            'all_teachers': teachers,
+            'sorted_teacher': sorted_teacher,
+            'sort': sort,
+        })
+
+
+class TeacherDetailView(View):
+    def get(self, request, teacher_id):
+        teacher = Teacher.objects.get(id=int(teacher_id))
+        all_courses = Course.objects.filter(teacher=teacher)
+
+        has_teacher_faved = False
+
+        if UserFavorite.objects.filter(user=request.user, fav_type=3, fav_id=teacher.id):
+            has_teacher_faved = True
+
+        has_org_faved = False
+        if UserFavorite.objects.filter(user=request.user, fav_type=2, fav_id=teacher.org.id):
+            has_org_faved = True
+
+        # 讲师排行前五名
+        sorted_teacher = Teacher.objects.all().order_by('-click_nums')[:5]
+        return render(request, 'teacher-detail.html', {
+            'teacher': teacher,
+            'all_courses': all_courses,
+            'sorted_teacher': sorted_teacher,
+            'has_teacher_faved': has_teacher_faved,
+            'has_org_faved': has_org_faved,
+        })
